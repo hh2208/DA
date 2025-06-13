c3fa93ae4ced7ad64731e570a8cf86d42085db39
hh2208 <tranthehien9812@gmail.com>
2025-06-13 07:52:45 +0700

    Test

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use <Img:sizes></Img:sizes>
 
 define('LARAVEL_START', microtime(true));
 
@@ -18,3 +19,5 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+
+
